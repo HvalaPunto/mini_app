ae858b21451aec8d40fa47ac3ff481fc8353a15a
Mirong1707 <ghanin03@list.ru>
2023-11-16 23:01:44 +0300

    test

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript для обработки нажатия на кнопку
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("message").innerHTML = "Привет, мир!";
+});
