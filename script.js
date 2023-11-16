ccf52689426fa0f1806be285a793530cb86742c6
Mirong1707 <ghanin03@list.ru>
2023-11-17 00:09:45 +0300

    test

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var contentDiv = document.getElementById('content');
-    var changeContentButton = document.getElementById('changeContent');
-
-    changeContentButton.addEventListener('click', function () {
-        var newContent = '<p>Content changed dynamically!</p>';
-        contentDiv.innerHTML = newContent;
-    });
-});
